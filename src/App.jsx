38a925cac7c9a148a2e2120294c99f29d37710ef
Mogilisetty Ranjith Kumar <ranjithmogili5@gmail.com>
2025-12-23 22:40:28 +0530

    completed TaskForm Component
    added functionality for the task form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import TaskForm from "./Components/TaskForm";
 import TaskList from "./Components/TaskList";
 import ProgressTracker from "./Components/ProgressTracker";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
+  });
+
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
+
   return(
     <div>
       <h1>CheisIt</h1>
       <p>Chase what is needed!</p>
-      <TaskForm />
+      <TaskForm addTask = {addTask}/>
       <TaskList />
       <ProgressTracker />
       <button>ClearAll</button>
